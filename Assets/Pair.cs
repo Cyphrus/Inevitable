e894c6e1e068b9e4e90b2cf90fdb11bc2ea59537
Cyphrus <wmaynes@cmu-875306.wv.cc.cmu.edu>
2015-05-06 16:59:07 -0400

    Actually Started Movies
    Shits getting done

---
+++ Assets/Pair.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pair<T1, T2>{
+
+	private readonly T1 first;
+
+	private readonly T2 second;
+
+	public T1 getFirst {
+		get { return first; }
+	}
+
+	public T2 getSecond {
+		get { return second; }
+	}
+
+	public Pair (T1 firstItem, T2 secondItem) {
+		this.first = firstItem;
+		this.second = secondItem;
+	}
+}
